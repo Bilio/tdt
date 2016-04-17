d01b02a3ef2c0f6b05c43a87cf18fbf66f6dca24
Dinesh P S <psdnsh@hotmail.com>
2016-04-16 19:25:54 -0700

    removed unwanted

--- scripts/tdt_train.py
+++
@@ -1,9 +0,0 @@
-import os
-import config
-corpusInfo = {}
-for dir in os.listdir(config.TDT_CORPUS_DIR):
-    corpusInfo[dir] = []
-    for doc in os.listdir(os.path.join(config.TDT_CORPUS_DIR, dir)):
-        corpusInfo[dir].append(doc)
-
-print corpusInfo
