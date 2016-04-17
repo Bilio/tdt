4b1aec506949de7a980a5137e9525196a5e6f365
username <vibhabhambhani@gmail.com>
2016-04-17 15:53:02 -0700

    Updated

--- scripts/Attempt007.py
+++ scripts/Attempt007.py
@@ -1,5 +1,6 @@
 import pickle
 import os
+import math
 '''
  For one topic do the following:
  1) represent the topic using the topic vector
@@ -23,6 +24,21 @@ def tokenizeAndTopicVectorCreation(fileoutput,T,tfRaw):
 
     return DVector,T,length
 
+def tokenizeAndDocumentVectorCreation(fileoutput,tfRawD):
+    words = fileoutput.split()
+    length = len(words)
+    uniqueWordsInDoc = set()
+    for word in words:
+        uniqueWordsInDoc.add(word)
+        if word not in tfRawD:
+            tfRawD[word]=1
+        else:
+            tfRawD[word]+=1
+    for i in tfRawD:
+      tfRawD[i]=float(tfRawD[i])/length
+
+    return uniqueWordsInDoc,words,length,tfRawD
+
 def extractVocab(V,DVector):
  #print(DVector)
  while DVector.__len__()!=0:
@@ -35,9 +51,78 @@ def extractVocab(V,DVector):
 
 #def dfUpdate(V,DVector):
 
+def calcluatetfD(tfRawD,lenD,lenAvg):
+    tfD ={}
+    print("Calc tfRawD")
+    print(tfRawD)
+    for i in tfRawD:
+            tfD[i]=float(tfRawD[i])/(tfRawD[i]+0.5+1.5*(lenD/lenAvg))
+    return tfD
+
+def calculatetfT(tfRaw,length):
+    tfT={}
+    for i in tfRaw:
+            tfT[i]=float(tfRaw[i])/(tfRaw[i]+0.5+1.5*(length/lenAvg))
+    return tfT
+
+
+def calculateidf(V,N):
+    idf={}
+    for word in V:
+        if (1.0*N/V[word])==1.0:
+            Num=(1.0*N/V[word])-0.0001
+        else:
+            Num=(1.0*N/V[word])
+        idf[word]=math.log(Num,10)/math.log((N+1),10)
+    return idf
+
+
+def CalcProductDoc(tfD, idf):
+    print("For dh calc")
+    print (tfD)
+    Dh ={}
+    for word in tfD:
+            Dh[word]=tfD[word]*idf[word]
+    return Dh
+def CalcProductTopic(tfT,idf):
+    Th ={}
+    for word in tfT:
+            Th[word]=tfT[word]*idf[word]
+    return Th
+
+def similarity(Dh,Th):
+    print("Inside similarity")
+    print(Dh)
+    print(Th)
+    Dh_Th=0
+    DhSquared=0
+    ThSquared=0
+    for word in V:
+        if word in Dh:
+            Dvalue=Dh[word]
+        else:
+            Dvalue=0
+        if word in Th:
+            Tvalue=Th[word]
+        else:
+            Tvalue=0
+
+        Dh_Th+=1.0*Dvalue*Tvalue
+        #print(Dh_Th)
+        #print(Dh_Th)
+        DhSquared+=1.0*Dvalue*Dvalue
+        print(DhSquared)
+        ThSquared+=1.0*Tvalue*Tvalue
+        #print(DhSquared)
+        #print(ThSquared)
+    cosineDeno=math.sqrt(DhSquared*ThSquared)
+    print(cosineDeno)
+    print("CD above")
+    similarity=float(Dh_Th)/cosineDeno*1.0
+    return similarity
 
 topicsFolder = "/Users/vibhabhambhani/Desktop/NLP/Project/trainingData"
-
+docs=[]
 #Define folder for topic outputs
 topicsFile=open("topics.txt","w")
 #Read a folder containing 4 documents of the topic
@@ -48,7 +133,7 @@ for topic in listOfTopics:
   tfRaw={}
   print(topic)
   z = os.path.join(topicsFolder,topic)
-  docs=[]
+
   V={}
   for item in os.listdir(z):
    if not item.startswith('.') and os.path.isfile(os.path.join(z, item)):
@@ -68,11 +153,54 @@ for topic in listOfTopics:
   #print("Vocabulary")
   #print(V)
   print("Length")
-  lenAvg=lengthofdocs/2
-  N=2
+  lenAvg=lengthofdocs
+  N=1
   for i in tfRaw:
       tfRaw[i]=float(tfRaw[i])/lengthofdocs
   dictionaryOfTopics.append({'topic':topic,'T':T,'V':V,'lenAvg':lenAvg,'N':N,'tfRaw':tfRaw,'length':lengthofdocs})
+
+for topic in dictionaryOfTopics:
+    similarityValueSum=0
+    similarityValue=0
+    for doc in docs:
+        print("Docd")
+        print(doc)
+        DVector=[]
+        uniqueWordsInDoc=set()
+        tfRawD={}
+        fileop = open(doc,"r")
+        uniqueWordsInDoc,DVector,lenD,tfRawD=tokenizeAndDocumentVectorCreation(fileop.read(),tfRawD)
+        tfD={}
+        tfD = calcluatetfD(tfRawD,lenD,topic['lenAvg'])
+        print("tfD")
+        print(tfD)
+        #Calculated tfT --- tf of Topic
+        tfT={}
+        tfT = calculatetfT(topic['tfRaw'],topic['length'])
+        print("tfT")
+        print(tfT)
+        #update idf statistics
+        idf={}
+        idf = calculateidf(V,N)
+        print("idf")
+        print(idf)
+        #tf.idf for document
+
+        Dh={}
+        Dh = CalcProductDoc(tfD, idf)
+
+
+        #tf.idf for topic
+        Th={}
+        Th = CalcProductTopic(tfT,idf)
+
+        #Step3: Story similarity computation
+        similarityValue=similarity(Dh,Th)
+        print(similarityValue)
+        similarityValueSum+=similarityValue
+    similarityValueSum=similarityValueSum/topic['N']
+    print(similarityValueSum)
+    topic['Z']=similarityValueSum
 print(dictionaryOfTopics)
 pickle.dump(dictionaryOfTopics,topicsFile)
 
